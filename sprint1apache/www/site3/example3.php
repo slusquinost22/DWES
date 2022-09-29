6c15a6c90b6a04026682d0c353cd25011db102fd
Sara Lusquiños <Sara.lusquinos@gmail.com>
2022-09-29 11:45:26 +0200

    Añadido ejercicio3 php

---
+++ sprint1apache/www/site3/example3.php
@@ -0,0 +1,18 @@
+<html>
+	<body>
+		<h1>Jubilaci&oacute;n</h1>
+		<?php
+			//function edad_en_10_anos ($edad) {
+			//	return $edad + 10;
+			//}
+
+			if ( $_GET["edad"] + 10 < 65) {
+				echo "En 10 a&ntilde;os tendr&aacute; edad de jubilaci&oacute;n";
+			} 	else {
+				echo "¡Disfruta de tu tiempo!";
+			}
+
+		?>
+	</body>
+</html>
+
