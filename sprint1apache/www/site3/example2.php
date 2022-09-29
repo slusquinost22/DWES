00aa30973c53c6fad469ed5cd3b03882cc9abd10
Sara Lusquiños <Sara.lusquinos@gmail.com>
2022-09-29 10:47:28 +0200

    Añadido ejercicio2 de PHP

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<h1>P&aacute;gina de bienvenidas </h1>
+		<?php
+			function dar_bienvenida ($nombre) {
+			echo "¡Bienvenido/a, ".$nombre."!";
+			}
+		
+			dar_bienvenida("Jared Leto");
+		?>
+	</body>
+</html>
