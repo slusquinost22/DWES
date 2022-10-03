7d2d36b08590471448cf3e1453fe0ead43400a16
Sara Lusquiños <Sara.lusquinos@gmail.com>
2022-10-03 13:04:28 +0200

    prueba

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -8,8 +8,12 @@
 			if ($_POST["funidad"] == "pulgada") {
 			$v_pulgadas = $_POST["fcantidad"];
 			$v_metros = $v_pulgadas * 0.0254;
-			echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
-			} else {
+				echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
+			} elseif ($_POST["fundidad"] == "yarda" {
+			$v_yarda = $_POST["fcantidad"];
+			$v_metros = $v_yarda * 0.9144;
+				echo $v_yarda."yarda(s) = ".$v_metros." metro(s)";
+			}else {
 			echo "Unidad no soportada";
 			}
 		}
@@ -21,6 +25,8 @@
 			<input type="text" id="cantidad_input" name="fcantidad"><br>
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>
+			<input type="radio" id="yarda_input" name="funidad" value="yarda">
+			<label for="yarda_input">Yarda(s)</label><br>
 			<input type="radio" id="otro_input" name="funidad" value="otro">
 			<label for="otro_input">Otro</label><br>
 		<input type="submit" value="Convertir">
