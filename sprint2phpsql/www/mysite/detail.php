14284365f68b8c5fa0756183c2075839d6a74575
slusquinost22 <113433522+slusquinost22@users.noreply.github.com>
2022-10-06 12:50:50 +0200

    Update detail.php

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -99,13 +99,14 @@
 			?>
 		
 			</div>
-		</div>
-		<p>Deja un nuevo comentario:</p>
+			<p>Deja un nuevo comentario:</p>
 			<form action="/comment.php" method="post">
 			<textarea rows="4" cols="50" name="new_comment"></textarea><br>
 			<input type="hidden" name="pelicula_id" value="<?php echo $pelicula_id; ?>">
 			<input type="submit" value="Comentar">
 			</form>
+		</div>
+		
 
 	</body>
 </html>
