d6c171416f12e6f0d72750c30010c8ca084b1e97
slusquinost22 <113433522+slusquinost22@users.noreply.github.com>
2022-10-06 12:17:45 +0200

    Update detail.php

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -89,7 +89,7 @@
 			$result2 = mysqli_query($db, $query2) or die('Query error');
 			while ($row = mysqli_fetch_array($result2)) {
 			echo '<br>';
-			echo "<td margin-left: 50px;><ul><li>$row[1]</li></ul></td>";
+			echo "<td><ol><li>$row[1]</li></ol></td>";
 			echo '<br>';
 			echo "</tr>";
 			echo "</table>";
