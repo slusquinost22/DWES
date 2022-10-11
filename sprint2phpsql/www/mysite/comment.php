03a4e012c77a1154e6465608d71398d8731a8f7a
slusquinost22 <113433522+slusquinost22@users.noreply.github.com>
2022-10-11 14:46:12 +0200

    Create comment.php

---
+++ sprint2phpsql/www/mysite/comment.php
@@ -0,0 +1,19 @@
+<?php
+  $db = mysqli_connect('localhost', 'root', '1234', 'web_canciones') or die('Fail');
+?>
+ <html>
+  <body>
+    <?php
+      $pelicula_id = $_POST['pelicula_id'];
+      $comentario = $_POST['new_comment'];
+      $query = "INSERT INTO tComentarios(comentario, pelicula_id, usuario_id)
+      VALUES ('".$comentario."',".$pelicula_id.",NULL)";
+      mysqli_query($db, $query) or die('Error');
+      echo "<p>Nuevo comentario ";
+      echo mysqli_insert_id($db);
+      echo " añadido</p>";
+      echo "<a href='/detail.php?pelicula_id=".$pelicula_id."'>Volver</a>";
+      mysqli_close($db);
+    ?>
+  </body>
+</html>
