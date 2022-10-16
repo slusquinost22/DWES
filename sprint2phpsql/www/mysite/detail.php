2466747810c9c6fe98f8b0576aad7ce2fc48ca4c
slusquinost22 <113433522+slusquinost22@users.noreply.github.com>
2022-10-16 22:35:35 +0200

    Update detail.php

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -111,7 +111,7 @@
 			$result2 = mysqli_query($db, $query2) or die('Query error');
 			while ($row = mysqli_fetch_array($result2)) {
 			echo '<br>';
-			echo Comentarios: ;
+			echo "Comentarios: ";
 			echo '<br>';
 			echo '<li>'.$row[1].'</li>';
 			echo '<br>';
