9c20b2dcde244d6becdbc56527a222edf0b8c3b4
slusquinost22 <113433522+slusquinost22@users.noreply.github.com>
2022-10-18 21:05:24 +0200

    Add files via upload

---
+++ sprint2python/factorial2.py
@@ -0,0 +1,13 @@
+factorial= int(input(str("CALCULA EL FACTORIAL." "\n""Introduce un número: ")))
+
+
+
+def func_factorial2(factorial):
+    if factorial==0 or factorial==1:
+        factorial=1
+    elif factorial >1:
+        for i in range(1, factorial):
+            factorial=factorial*i
+    return factorial
+
+print(str("El factorial de"),factorial, str("es:" ),func_factorial2(factorial))
