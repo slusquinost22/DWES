6ec4c8730d437fb95f7a495c7473c1a0e22c06a8
Sara Lusquiños <Sara.lusquinos@gmail.com>
2022-10-18 20:59:07 +0200

    COMPLETADO EJERCICIO 3 PYTHON

--- sprint2python/factorial.py
+++ sprint2python/factorial.py
@@ -1,7 +1,9 @@
 def func_factorial(factorial):
    if factorial==0 or factorial==1:
             factorial=1
+
    elif factorial>1:
             factorial=factorial*func_factorial(factorial-1)
+
    return factorial
 
