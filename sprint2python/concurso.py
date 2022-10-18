e5adef628658bc532a5e84e4aea4193d617e6ec8
slusquinost22 <113433522+slusquinost22@users.noreply.github.com>
2022-10-18 20:41:51 +0200

    Update concurso.py

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,10 +1,33 @@
 ﻿# -*- coding: utf-8 -*-
+resultado = 0
 print("¿En qué año se descubrió América?\n" 
     "1) 1492\n" 
     "2) 1354\n" 
     "3) 1942")
 respuesta = int(input("Introduce una opción: "))
 if respuesta == 1:
-    print("¡CORRECTO!")
+    resultado +=10
 else:
-    print("¡HAS FALLADO!")
+    resultado -=5
+
+print ("¿Cuántos planetas existen?\n"
+    "1) 9\n"
+    "2) 5\n"
+    "3) 8\n")
+respuesta = int(input("Introduce una opción: "))
+if respuesta == 3:
+    resultado +=10
+else:
+    resultado -=5
+    
+print ("¿Cuántos meses tienen 28 días?\n"
+    "1) 1\n"
+    "2) 12\n"
+    "3) 6\n")
+respuesta = int(input("Introduce una opción: "))
+if respuesta == 2:
+    resultado +=10
+else:
+    resultado -=5
+
+print ("Tu puntuación es: " + str(resultado))
