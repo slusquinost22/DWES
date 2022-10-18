835eb0906a941bbafd4711d0676b0f3cadb5771d
slusquinost22 <113433522+slusquinost22@users.noreply.github.com>
2022-10-18 20:56:32 +0200

    Add files via upload

---
+++ sprint2python/principal.py
@@ -0,0 +1,5 @@
+from factorial import func_factorial
+
+factorial= int(input(str("CALCULA EL FACTORIAL." "\n""Introduce un número: ")))
+
+print(str("El factorial de"),factorial, str("es:" ),func_factorial(factorial))
