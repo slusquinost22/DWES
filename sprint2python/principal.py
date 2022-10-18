dd529239728bb87a8b62d7524e235e4bad364672
slusquinost22 <113433522+slusquinost22@users.noreply.github.com>
2022-10-18 21:21:42 +0200

    Update principal.py

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,4 +1,3 @@
-import time
 from factorial import func_factorial
 from factorial2 import func_factorial2
 
@@ -7,21 +6,10 @@ print("b) Calcular factorial sin recursividad")
 
 opcion= input("Escoge una opción: ")
 
-factorial= int(input("Introduce un número: ")))
+factorial= int(input("Introduce un número: "))
 
 if opcion == 'a':
-    start_time = time.time()
-    print(func_factorial(factorial))
-    end_time = time.time()
-    elapsed_time = end_time -start_time
-    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
     print(str("El factorial de"),factorial, str("es:" ),func_factorial(factorial))
-
 else:
-     start_time = time.time()
-    print(func_factorial2(factorial))
-    end_time = time.time()
-    elapsed_time = end_time -start_time
-    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
     print(str("El factorial de"),factorial, str("es:" ),func_factorial2(factorial))
 
