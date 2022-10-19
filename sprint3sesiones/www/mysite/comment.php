6550b4257a24865a15452c415a812f80b4a63d24
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2022-10-19 19:06:44 +0200

    COMPLETADO EJERCICIO 4 Y CORRECCIÓN ERROR EN MAIN.PHP

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -7,12 +7,12 @@
 	session_start();
 	$user_id_a_insertar = 'NULL';
 	if (!empty($_SESSION['user_id'])) {
-		$user_id_a_insertar = $_SESSION['user_id'};
+		$user_id_a_insertar = $_SESSION['user_id'];
 	}
       $pelicula_id = $_POST['pelicula_id'];
       $comentario = $_POST['new_comment'];
       $query = "INSERT INTO tComentarios(comentario, pelicula_id, usuario_id, fecha)
-      VALUES ('".$comentario."',".$pelicula_id.",NULL, NOW())";
+      VALUES ('".$comentario."',".$pelicula_id.",".$user_id_a_insertar.", NOW())";
       mysqli_query($db, $query) or die('Error');
       echo "<p>Nuevo comentario ";
       echo mysqli_insert_id($db);
