496584ceade3aaf6f31555d68908c68038643abd
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2022-11-13 12:26:25 +0100

    CREADA PRIMERA PÁGINA WEB DE PRUEBA

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def pagina_de_prueba(request):
+	return HttpResponse("<h1>Hola caracola</h1>");
+
