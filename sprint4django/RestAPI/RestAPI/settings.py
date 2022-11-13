8180a127ac26b08d5dcb42fa865381c01e04fa48
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2022-11-13 11:56:09 +0100

    Primer paso: Crear una app, COMPLETADO

--- sprint4django/RestAPI/RestAPI/settings.py
+++ sprint4django/RestAPI/RestAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webserviceapp.apps.WebserviceappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
