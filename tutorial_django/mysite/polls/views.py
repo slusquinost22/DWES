a486f9ad16756cf388c4e63dc5c8af149ab0f437
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2022-11-20 17:18:07 +0100

    TUTORIAL 3.3: 1-Creada vista para Error404 con get_object_or_404() en polls/views.py. 2-Creado archivo polls/templates/polls/detail.html y creada plantila.

--- tutorial_django/mysite/polls/views.py
+++ tutorial_django/mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
@@ -12,7 +12,8 @@ def index(request):
 	return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-	return HttopResponse("You're looking at question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', { 'question': question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
