07cc084a2fe30e89114190e1311dc4c86edf4903
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2022-11-20 18:10:44 +0100

    TUTORIAL 4.2: 1-Modificada vista results() para que muestre los resultados de las votaciones. 2-Creado archivo results.html en polls/templates/polls. 3-Comprobado que funcionan las votaciones y los resultados en el navegador.

--- tutorial_django/mysite/polls/views.py
+++ tutorial_django/mysite/polls/views.py
@@ -17,11 +17,11 @@ def detail(request, question_id):
 	return render(request, 'polls/detail.html', { 'question': question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html',{'question': question})
 
 def vote(request, question_id):
-	question = get_object_or_404(Question, pk=question.id)
+	question = get_object_or_404(Question, pk=question_id)
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
